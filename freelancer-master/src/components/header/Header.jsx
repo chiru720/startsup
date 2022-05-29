0e855ba4d969f692ae96c52c110389db7cfadce1
chiru-sit <chiranjib.mukherjee450@gmail.com>
2022-05-29 16:57:03 +0530

    first base

--- freelancer-master/src/components/header/Header.jsx
+++ freelancer-master/src/components/header/Header.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './header.scss';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export default function Header() {
   return (
-    <div>component from header</div>
-  )
+    <div className="header-container">
+      <div className="container">
+        <div className="menu">
+          <div className="logo">logo here</div>
+          <div className="menu-items">menu here</div>
+        </div>
+        <div className="header-content">
+          <div className="hero-text">
+            <div className="hero-text-title">
+              <Typography variant="h3">
+                We design amazing experience for you
+              </Typography>
+            </div>
+            <div className="hero-text-subheading">
+              <Typography
+                sx={{ letterSpacing: '2px', wordSpacing: '2px' }}
+                variant="subtitle1"
+              >
+                Template is a professional webflow with multiple sections.
+              </Typography>
+            </div>
+            <div className="hero-button">
+              <Button variant="contained" size="medium">
+                Get started
+              </Button>
+              <Button variant="contained" size="medium">
+                Learn More
+              </Button>
+            </div>
+          </div>
+          <div className="hero-image">zszsddzd</div>
+        </div>
+      </div>
+    </div>
+  );
 }

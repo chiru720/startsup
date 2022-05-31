4ee389b13ed867c2c7f4d73d09e2ccc436b0f4c9
chiru-sit <chiranjib.mukherjee450@gmail.com>
2022-05-31 21:09:51 +0530

    31/05/22

--- freelancer-master/src/components/contactus/Contactus.jsx
+++ freelancer-master/src/components/contactus/Contactus.jsx
@@ -1,7 +1,104 @@
-import React from 'react'
+import React from 'react';
+import './contactus.scss';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
 
 export default function Contactus() {
   return (
-    <div>component from contact us</div>
-  )
+    <div className="contactus">
+      <Typography
+        className="tag"
+        variant="overline"
+        display="block"
+        gutterBottom
+      >
+        Contact us
+      </Typography>
+      <Typography
+        className="contactus-heading"
+        variant="h4"
+        gutterBottom
+        component="div"
+      >
+        Contact us and get your business online
+      </Typography>
+      <Typography className="contactus-body" variant="body2" gutterBottom>
+        We recommended you to contact us and get best quotation. Get ready with
+        your website in no time
+      </Typography>
+      <div className="form">
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              id="standard-name-input"
+              label="Name"
+              type="text"
+              variant="standard"
+              autoComplete="off"
+              helperText=""
+            />
+            <TextField
+              id="standard-email-input"
+              label="Email"
+              type="email"
+              variant="standard"
+              autoComplete="off"
+              helperText=""
+            />
+            <TextField
+              id="standard-phone-input"
+              label="Phone No"
+              type="number"
+              variant="standard"
+              autoComplete="off"
+              helperText=""
+            />
+            <TextField
+              id="standard-search"
+              label="Services"
+              type="search"
+              variant="standard"
+              autoComplete="off"
+            />
+          </div>
+        </Box>
+        <TextareaAutosize
+          id="standard-name-input"
+          aria-label="empty textarea"
+          placeholder="Tell us about your requirement"
+          style={{
+            width: 400,
+            height: 100,
+            margin: 10,
+            padding: 10,
+            boxSizing: 'border - box',
+          }}
+        />
+        <div className="submit-button">
+          <Button
+            sx={{
+              background: '#50b474',
+              padding: '8px 14px',
+              width: '10rem',
+              borderRadius: '20px',
+            }}
+            variant="contained"
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- freelancer-master/src/components/footer/Footer.jsx
+++ freelancer-master/src/components/footer/Footer.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import './footer.scss';
 
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-text">
+        Copyright. Situp 2022. All rights reserved.
+      </div>
+      <div className="footer-icons">
+        <span className="footer-icons-items">
+          <InstagramIcon fontSize="small" />
+        </span>
+        <span className="footer-icons-items">
+          <FacebookIcon fontSize="small" />
+        </span>
+        <span className="footer-icons-items">
+          <TwitterIcon fontSize="small" />
+        </span>
+        <span className="footer-icons-items">
+          <LinkedInIcon fontSize="small" />
+        </span>
+      </div>
+    </div>
+  );
 }
